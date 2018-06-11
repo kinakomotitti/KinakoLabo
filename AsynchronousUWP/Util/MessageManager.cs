988cd4e0f9fe097d431e47e417c50c36cfbf9d42
きなこもちっち <kinakomotitti0210@yahoo.co.jp>
2018-06-11 23:54:31 +0900

    キャンセル実装途中・・・

---
+++ AsynchronousUWP/Util/MessageManager.cs
@@ -0,0 +1,35 @@
+﻿namespace AsynchronousUWP.Util
+{
+    #region using
+    using System;
+    using System.Threading;
+    #endregion
+
+    public static class MessageManager
+    {
+        #region public
+
+        public static string WriteStart([System.Runtime.CompilerServices.CallerMemberName]string methodName = "")
+        {
+            return ConsoleWriteLineCore(methodName, "Start");
+
+        }
+
+        public static string WriteEnd([System.Runtime.CompilerServices.CallerMemberName]string methodName = "")
+        {
+            return ConsoleWriteLineCore(methodName, "End");
+        }
+
+        public static string WriteJobStatus(string message, [System.Runtime.CompilerServices.CallerMemberName]string methodName = "")
+        {
+            return ConsoleWriteLineCore(methodName, message);
+        }
+
+        #endregion
+
+        private static string ConsoleWriteLineCore(string methodName, string message)
+        {
+            return $"Thread ID :{Thread.CurrentThread.ManagedThreadId} ,{methodName} {message}";
+        }
+    }
+}
--- AsynchronousUWP/Views/AsynchronousPage.xaml.cs
+++ AsynchronousUWP/Views/AsynchronousPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using AsynchronousUWP.Util;
+using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 
 namespace AsynchronousUWP.Views
@@ -27,5 +30,64 @@ namespace AsynchronousUWP.Views
         }
 
         private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private Task<int> SampleTask = null;
+        private CancellationTokenSource source = null;
+        private void Execute_Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.source = new CancellationTokenSource();
+            this.SampleTask =  Task.Run<int>(() =>
+            {
+                return SampleMethod(this.source.Token);
+            });
+
+
+            this.SampleTask.ContinueWith((Task,param) =>
+            {
+                
+                this.TaskJobStatus_TextBlock.Text = MessageManager.WriteJobStatus("OnlyOnFaulted");
+            }, this.source.Token, TaskScheduler.FromCurrentSynchronizationContext());
+
+            //TaskScheduler.Defaultを指定すると、ThradPoolのスレッドでタスクが実行されるため、以下の例外がスローされる。
+            //System.Exception: 'アプリケーションは、別のスレッドにマーシャリングされたインターフェイスを呼び出しました。
+            //this.SampleTask.ContinueWith((Task) =>
+            //{
+            //    this.TaskJobStatus_TextBlock.Text = "canceled";
+            //},this.source.Token, TaskContinuationOptions.OnlyOnCanceled, TaskScheduler.Default);
+
+            this.SampleTask.ContinueWith((Task) =>
+            {
+                this.TaskJobStatus_TextBlock.Text = MessageManager.WriteJobStatus("RanToCompletion");
+            }, this.source.Token, TaskContinuationOptions.NotOnCanceled, TaskScheduler.FromCurrentSynchronizationContext());
+
+        }
+
+        private void Cancel_Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.source.Cancel();
+        }
+        private int SampleMethod(CancellationToken token)
+        {
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Run(async () =>
+                {
+                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        //ユーザーインターフェースを操作する
+                        this.TaskStatus_TextBlock.Text = $"ほげほげ{i}";
+                    });
+                });
+                Thread.Sleep(1000);
+                sum += i;
+                if (token.IsCancellationRequested)
+                {
+                    token.ThrowIfCancellationRequested();
+                }
+            }
+            return sum;
+        }
     }
 }
+
