ffa202d2d69a5531b8b528a20e3bbdf7f24098ec
きなこもちっち <kinakomotitti0210@yahoo.co.jp>
2018-08-13 00:16:52 +0900

    15/23完了

--- Patterns/No23_Visitor_Pattern.cs
+++ Patterns/No23_Visitor_Pattern.cs
@@ -1,132 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Patterns
+﻿namespace Patterns
 {
+    #region using
+    using System;
+    using System.Collections.Generic;
+    #endregion
+
+    /// <summary>
+    /// 「データ構造」と「それに対する処理」を分離することを目的とするパターンです。
+    /// </summary>
     class No23_Visitor_Pattern
     {
         static void Main()
-
         {
-
-            // Setup employee collection
-
             Employees e = new Employees();
-
             e.Attach(new Clerk());
-
             e.Attach(new Director());
-
             e.Attach(new President());
 
-
-
-            // Employees are 'visited'
-
             e.Accept(new IncomeVisitor());
-
             e.Accept(new VacationVisitor());
-
-
-
-            // Wait for user
-
-            Console.ReadKey();
-
         }
-
-
-
-
-        /// <summary>
-
-        /// The 'Visitor' interface
-
-        /// </summary>
 
         interface IVisitor
-
         {
-
             void Visit(Element element);
-
         }
 
-
-
-        /// <summary>
-
-        /// A 'ConcreteVisitor' class
-
-        /// </summary>
+        #region Visitor
 
         class IncomeVisitor : IVisitor
-
         {
-
             public void Visit(Element element)
-
             {
-
-                Employee employee = element as Employee;
-
-
-
-                // Provide 10% pay raise
-
+                BaseEmployee employee = element as BaseEmployee;
                 employee.Income *= 1.10;
-
-                Console.WriteLine("{0} {1}'s new income: {2:C}",
-
-                  employee.GetType().Name, employee.Name,
-
-                  employee.Income);
-
+                Console.WriteLine("{0} {1}'s new income: {2:C}", employee.GetType().Name, employee.Name, employee.Income);
             }
-
         }
-
-
-
-        /// <summary>
-
-        /// A 'ConcreteVisitor' class
-
-        /// </summary>
 
         class VacationVisitor : IVisitor
-
         {
-
             public void Visit(Element element)
-
             {
-
-                Employee employee = element as Employee;
-
-
-
-                // Provide 3 extra vacation days
-
+                BaseEmployee employee = element as BaseEmployee;
                 employee.VacationDays += 3;
-
-                Console.WriteLine("{0} {1}'s new vacation days: {2}",
-
-                  employee.GetType().Name, employee.Name,
-
-                  employee.VacationDays);
-
+                Console.WriteLine("{0} {1}'s new vacation days: {2}", employee.GetType().Name, employee.Name, employee.VacationDays);
             }
-
         }
 
+        #endregion
 
-
-        /// <summary>
-
-        /// The 'Element' abstract class
-
-        /// </summary>
+        #region Employee
 
         abstract class Element
 
@@ -136,207 +60,87 @@ namespace Patterns
 
         }
 
-
-
-        /// <summary>
-
-        /// The 'ConcreteElement' class
-
-        /// </summary>
-
-        class Employee : Element
-
+        class BaseEmployee : Element
         {
-
             private string _name;
-
             private double _income;
-
             private int _vacationDays;
 
-
-
-            // Constructor
-
-            public Employee(string name, double income,
-
-              int vacationDays)
-
+            public BaseEmployee(string name, double income, int vacationDays)
             {
-
                 this._name = name;
-
                 this._income = income;
-
                 this._vacationDays = vacationDays;
-
             }
-
-
-
-            // Gets or sets the name
 
             public string Name
-
             {
-
                 get { return _name; }
-
                 set { _name = value; }
-
             }
-
-
-
-            // Gets or sets income
 
             public double Income
-
             {
-
                 get { return _income; }
-
                 set { _income = value; }
-
             }
-
-
-
-            // Gets or sets number of vacation days
 
             public int VacationDays
-
             {
-
                 get { return _vacationDays; }
-
                 set { _vacationDays = value; }
-
             }
-
-
 
             public override void Accept(IVisitor visitor)
-
             {
-
+                //TODO 「POCOとビジネスプロセスの分離の表現」
+                //「データ構造」と「それに対する処理」が分離する瞬間。
+                //「データ構造」が自分自身を「処理」に引き渡す。
                 visitor.Visit(this);
-
             }
-
         }
 
+        #endregion
 
+        #region 個々のEmployee情報クラス
+        class Clerk : BaseEmployee
+        {
+            public Clerk() : base("Hank", 25000.0, 14) { }
+        }
 
-        /// <summary>
+        class Director : BaseEmployee
+        {
+            public Director() : base("Elly", 35000.0, 16) { }
+        }
 
-        /// The 'ObjectStructure' class
-
-        /// </summary>
+        class President : BaseEmployee
+        {
+            public President() : base("Dick", 45000.0, 21) { }
+        }
+        #endregion
 
         class Employees
-
         {
+            private List<BaseEmployee> _employees = new List<BaseEmployee>();
 
-            private List<Employee> _employees = new List<Employee>();
-
-
-
-            public void Attach(Employee employee)
-
+            public void Attach(BaseEmployee employee)
             {
-
                 _employees.Add(employee);
-
             }
 
-
-
-            public void Detach(Employee employee)
-
+            public void Detach(BaseEmployee employee)
             {
-
                 _employees.Remove(employee);
-
             }
-
-
 
             public void Accept(IVisitor visitor)
-
             {
-
-                foreach (Employee e in _employees)
-
+                foreach (BaseEmployee e in _employees)
                 {
-
                     e.Accept(visitor);
-
                 }
-
                 Console.WriteLine();
-
             }
-
         }
-
-
-
-        // Three employee types
-
-
-
-        class Clerk : Employee
-
-        {
-
-            // Constructor
-
-            public Clerk()
-
-              : base("Hank", 25000.0, 14)
-
-            {
-
-            }
-
-        }
-
-
-
-        class Director : Employee
-
-        {
-
-            // Constructor
-
-            public Director()
-
-              : base("Elly", 35000.0, 16)
-
-            {
-
-            }
-
-        }
-
-
-
-        class President : Employee
-
-        {
-
-            // Constructor
-
-            public President()
-
-              : base("Dick", 45000.0, 21)
-
-            {
-
-            }
-
-        }
-
     }
 }
