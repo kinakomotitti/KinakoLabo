068616e49f9141dabda17eba45fadcd22477c92f
きなこもちっち <kinakomotitti0210@yahoo.co.jp>
2018-08-14 22:33:29 +0900

    21/23完了

--- Patterns/No22_TemplateMethod_Pattern.cs
+++ Patterns/No22_TemplateMethod_Pattern.cs
@@ -1,212 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Patterns
+﻿namespace Patterns
 {
+    #region using
+    using System;
+    using System.Data;
+    using System.Data.OleDb;
+    #endregion
+
+    /// <summary>
+    /// 
+    /// </summary>
     class No22_TemplateMethod_Pattern
     {
         static void Main()
-
         {
-
             DataAccessObject daoCategories = new Categories();
-
             daoCategories.Run();
 
-
-
             DataAccessObject daoProducts = new Products();
-
             daoProducts.Run();
-
-
-
-            // Wait for user
-
-            Console.ReadKey();
-
         }
 
-
-
-        /// <summary>
-
-        /// The 'AbstractClass' abstract class
-
-        /// </summary>
-
+        //TODO 「Baseクラスを使って見ましょう的な感じ」
         abstract class DataAccessObject
-
         {
-
             protected string connectionString;
-
             protected DataSet dataSet;
 
-
-
             public virtual void Connect()
-
             {
-
-                // Make sure mdb is available to app
-
-                connectionString =
-
-                  "provider=Microsoft.JET.OLEDB.4.0; " +
-
-                  "data source=..\\..\\..\\db1.mdb";
-
+                connectionString =string.Empty;
             }
-
-
-
-            public abstract void Select();
-
-            public abstract void Process();
-
-
 
             public virtual void Disconnect()
-
             {
-
-                connectionString = "";
-
+                connectionString = string.Empty;
             }
 
-
-
-            // The 'Template Method' 
+            public abstract void Select();
+            public abstract void Process();
 
             public void Run()
-
             {
-
                 Connect();
-
                 Select();
-
                 Process();
-
                 Disconnect();
-
             }
-
         }
-
-
-
-        /// <summary>
-
-        /// A 'ConcreteClass' class
-
-        /// </summary>
 
         class Categories : DataAccessObject
-
         {
-
             public override void Select()
-
             {
-
                 string sql = "select CategoryName from Categories";
-
-                OleDbDataAdapter dataAdapter = new OleDbDataAdapter(
-
-                  sql, connectionString);
-
-
-
+                OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sql, connectionString);
                 dataSet = new DataSet();
-
                 dataAdapter.Fill(dataSet, "Categories");
-
             }
-
-
 
             public override void Process()
-
             {
-
                 Console.WriteLine("Categories ---- ");
-
-
-
                 DataTable dataTable = dataSet.Tables["Categories"];
-
                 foreach (DataRow row in dataTable.Rows)
-
                 {
-
                     Console.WriteLine(row["CategoryName"]);
-
                 }
-
                 Console.WriteLine();
-
             }
-
         }
-
-
-
-        /// <summary>
-
-        /// A 'ConcreteClass' class
-
-        /// </summary>
 
         class Products : DataAccessObject
-
         {
-
             public override void Select()
-
             {
-
                 string sql = "select ProductName from Products";
-
-                OleDbDataAdapter dataAdapter = new OleDbDataAdapter(
-
-                  sql, connectionString);
-
-
-
+                OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sql, connectionString);
                 dataSet = new DataSet();
-
                 dataAdapter.Fill(dataSet, "Products");
-
             }
-
-
 
             public override void Process()
-
             {
-
                 Console.WriteLine("Products ---- ");
-
                 DataTable dataTable = dataSet.Tables["Products"];
-
                 foreach (DataRow row in dataTable.Rows)
-
                 {
-
                     Console.WriteLine(row["ProductName"]);
-
                 }
-
                 Console.WriteLine();
-
             }
-
         }
-
     }
 }
